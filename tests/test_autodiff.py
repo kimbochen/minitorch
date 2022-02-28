af03e84869f4705894413223ced64163aeb56ad5
kimbochen <github@thc.anonaddy.com>
2022-02-28 15:44:50 +0800

    Fixed typo.

--- tests/test_autodiff.py
+++ tests/test_autodiff.py
@@ -22,7 +22,7 @@ class Function1(minitorch.ScalarFunction):
 class Function2(minitorch.ScalarFunction):
     @staticmethod
     def forward(ctx, x, y):
-        ":math:`f(x, y) = x \timex y + x`"
+        ":math:`f(x, y) = x \times y + x`"
         ctx.save_for_backward(x, y)
         return x * y + x
 
@@ -46,7 +46,7 @@ def test_chain_rule1():
 
 @pytest.mark.task1_3
 def test_chain_rule2():
-    "Check that constrants are ignored and variables get derivatives."
+    "Check that constants are ignored and variables get derivatives."
     var = minitorch.Variable(History())
     constant = minitorch.Variable(None)
     back = Function1.chain_rule(ctx=None, inputs=[var, constant], d_output=5)
@@ -59,7 +59,7 @@ def test_chain_rule2():
 
 @pytest.mark.task1_3
 def test_chain_rule3():
-    "Check that constrants are ignored and variables get derivatives."
+    "Check that constants are ignored and variables get derivatives."
     constant = 10
     var = minitorch.Scalar(5)
 
